49f315d4f3c4d888d3a3c78f4853457493806b4a
mpsanchis <33475618+mpsanchis@users.noreply.github.com>
2025-11-27 16:31:52 +0100

    Configure guess-indent with defaults, to recognise indentation of a file

--- init.lua
+++ init.lua
@@ -255,9 +255,13 @@ rtp:prepend(lazypath)
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
-  'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
+  {
+    'NMAC427/guess-indent.nvim', -- detect tabstop and shiftwidth automatically
+    config = function()
+      require('guess-indent').setup {}
+    end,
+  },
   'ThePrimeagen/vim-be-good',
-
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
